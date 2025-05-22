ac419ab0b42044c32eb575c244801bb0f62bcb8b
Nicholas Eastman <29488820+ironman820@users.noreply.github.com>
2025-05-22 21:05:02 +0000

    May 22, 2025, 4:05 PM

--- filterme.py
+++ filterme.py
@@ -12,7 +12,7 @@ def email_to_pandas(client: IMAPClient, folder: str = "INBOX") -> pd.DataFrame:
     uids: list = client.search()
     raw_messages: dict[int, dict] = client.fetch(uids, data=["BODY[]"])
     emails: pd.DataFrame = pd.DataFrame(raw_messages, columns=["uid", "message"])
-    print(raw_messages[uids[0]])
+    print(emails)
     exit()
     # emails["uid"] = emails[0]
     # emails.drop([0], axis="columns", inplace=True)
