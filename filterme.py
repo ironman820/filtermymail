d042d246235e4791cd85155a4ee94de61eca7c91
Nicholas Eastman <29488820+ironman820@users.noreply.github.com>
2025-05-22 21:16:33 +0000

    May 22, 2025, 4:16 PM

--- filterme.py
+++ filterme.py
@@ -21,7 +21,7 @@ def email_to_pandas(client: IMAPClient, folder: str = "INBOX") -> pd.DataFrame:
     #     )
     #     print(message.get_subject())
     #     break
-    emails["message"] = emails["b'BODY[]'"]
+    emails["message"] = emails[1]
     emails["message"].apply(
         lambda x: pd.Series(breakout_email_columns(x), index=["subject"]), axis=1
     )
